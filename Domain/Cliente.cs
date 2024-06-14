0a5c8361b9a6bac87169cb97007c8488218b6d56
João Vitor <joao.vitor@maxdata.com.br>
2024-06-14 10:48:28 -0300

    first commit

---
+++ Domain/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreSistemaDePedidos.Domain
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String Telefone { get; set; }
+        public String CEP { get; set; } 
+        public String Estado { get; set; }
+        public String Cidade { get; set; }
+    }
+}
---
+++ Domain/Pedido.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreSistemaDePedidos.Domain
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+       
+    }
+}
